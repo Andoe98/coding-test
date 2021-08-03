4c379c583fb7c634cfef6eb31681b145d5c03183
Andrew Jap <andrew.jap1@gmail.com>
2021-08-03 22:10:03 +1000

    Amended test for array

--- src/test/java/com/codingexercise/rest/EmployeeEndpointTest.java
+++ src/test/java/com/codingexercise/rest/EmployeeEndpointTest.java
@@ -7,6 +7,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -26,30 +30,52 @@ class EmployeeEndpointTest {
 	private MockMvc mockMvc;
 	private String endpoint = "/employee/payslip";
 	private ObjectMapper mapper = new ObjectMapper();
-	private Employee employee = new Employee (	
-			"Monica",
-			"Tan",
-			60050,
-			2,
-			0.09
+	private static List<Employee> employees = new ArrayList<>();
+	
+	@BeforeAll
+	static void beforeAll() {
+		employees.add(
+			new Employee (	
+				"Monica",
+				"Tan",
+				60050,
+				2,
+				0.09
+			)
 		);
+		employees.add(
+			new Employee (	
+				"Brend",
+				"Tulu",
+				120000,
+				1,
+				0.1
+			)
+		);
+	}
 	
 	@Test
 	void testPaySlips() throws Exception {
-		//TODO: amend test for JSON request array
 		ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
-	    String requestJson = ow.writeValueAsString(employee);
+	    String requestJson = ow.writeValueAsString(employees);
 	    
 	    mockMvc.perform(post(endpoint).contentType(MediaType.APPLICATION_JSON)
 	    		.content(requestJson))
 	    		.andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON))
-	    		.andExpect(jsonPath("$.data.employee", notNullValue()))
-	            .andExpect(jsonPath("$.data.fromDate", is("01 March")))
-	            .andExpect(jsonPath("$.data.toDate", is("31 March")))
-	            .andExpect(jsonPath("$.data.grossIncome", is(5004)))
-	            .andExpect(jsonPath("$.data.incomeTax", is(922)))
-	            .andExpect(jsonPath("$.data.superannuation", is(450)))
-	            .andExpect(jsonPath("$.data.netIncome", is(4082)));	    
+	    		.andExpect(jsonPath("$.data[0].employee", notNullValue()))
+	            .andExpect(jsonPath("$.data[0].fromDate", is("01 March")))
+	            .andExpect(jsonPath("$.data[0].toDate", is("31 March")))
+	            .andExpect(jsonPath("$.data[0].grossIncome", is(5004)))
+	            .andExpect(jsonPath("$.data[0].incomeTax", is(922)))
+	            .andExpect(jsonPath("$.data[0].superannuation", is(450)))
+	            .andExpect(jsonPath("$.data[0].netIncome", is(4082)))
+	            .andExpect(jsonPath("$.data[1].employee", notNullValue()))
+	            .andExpect(jsonPath("$.data[1].fromDate", is("01 February")))
+	            .andExpect(jsonPath("$.data[1].toDate", is("28 February")))
+	            .andExpect(jsonPath("$.data[1].grossIncome", is(10000)))
+	            .andExpect(jsonPath("$.data[1].incomeTax", is(2669)))
+	            .andExpect(jsonPath("$.data[1].superannuation", is(1000)))
+	            .andExpect(jsonPath("$.data[1].netIncome", is(7331)));	    
 	    
 	}
 	
