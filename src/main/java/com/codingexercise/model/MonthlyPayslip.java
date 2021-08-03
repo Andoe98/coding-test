b2b13971760def2e4897e2cc3e77a117c14804fe
Andrew Jap <andrew.jap1@gmail.com>
2021-08-03 15:10:28 +1000

    Implemented MonthlyPayslip Model

--- src/main/java/com/codingexercise/model/MonthlyPayslip.java
+++ src/main/java/com/codingexercise/model/MonthlyPayslip.java
@@ -1,7 +1,79 @@
 package com.codingexercise.model;
 
 public class MonthlyPayslip {
-	public MonthlyPayslip(Employee employee, String fromDate, String toDate, int grossIncome, int incomeTax, int supperannuation, int netIncome) {
-		
+	
+	Employee employee;
+	String fromDate;
+	String toDate;
+	int grossIncome;
+	int incomeTax;
+	int superAnnuation;
+	int netIncome;
+	
+	public MonthlyPayslip(Employee employee, String fromDate, String toDate, int grossIncome, int incomeTax, int superannuation, int netIncome) {
+		this.employee = employee;
+		this.fromDate = fromDate;
+		this.toDate = toDate;
+		this.grossIncome = grossIncome;
+		this.incomeTax = incomeTax;
+		this.superAnnuation = superannuation;
+		this.netIncome = netIncome;
 	}
+	
+	public Employee getEmployee() {
+		return employee;
+	}
+	
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	
+	public String getFromDate() {
+		return fromDate;
+	}
+	
+	public void setFromDate(String fromDate) {
+		this.fromDate = fromDate;
+	}
+	
+	public String getToDate() {
+		return fromDate;
+	}
+	
+	public void setToDate(String toDate) {
+		this.toDate = toDate;
+	}
+	
+	public int getGrossIncome() {
+		return grossIncome;
+	}
+	
+	public void setGrossIncome(int grossIncome) {
+		this.grossIncome = grossIncome;
+	}
+	
+	public int getIncomeTax() {
+		return incomeTax;
+	}
+	
+	public void setIncomeTax(int incomeTax) {
+		this.incomeTax = incomeTax;
+	}
+	
+	public int getSuperAnnuation() {
+		return superAnnuation;
+	}
+	
+	public void setSuperAnnuation(int superAnnuation) {
+		this.superAnnuation = superAnnuation;
+	}
+	
+	public int getNetIncome() {
+		return netIncome;
+	}
+	
+	public void setNetIncome(int netIncome) {
+		this.netIncome = netIncome;
+	}
+	
 }
