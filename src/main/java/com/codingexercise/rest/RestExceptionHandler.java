69fa1fca63524eafa80ede7a223ef7034a1836df
Andrew Jap <andrew.jap1@gmail.com>
2021-08-05 21:09:28 +1000

    Added error handling and response

---
+++ src/main/java/com/codingexercise/rest/RestExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.codingexercise.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+@SuppressWarnings({"unchecked", "rawtypes"})
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+	
+	@ExceptionHandler(Exception.class)
+    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+        List<String> details = new ArrayList<>();
+        details.add(ex.getLocalizedMessage());
+        ErrorResponse error = new ErrorResponse("Server Error", details);
+        return new ResponseEntity(error, HttpStatus.BAD_REQUEST);
+    }
+	
+}
