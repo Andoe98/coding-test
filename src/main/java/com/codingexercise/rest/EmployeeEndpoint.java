6f6812f053de255d90f4470e931dfc5737558bda
Andrew Jap <andrew.jap1@gmail.com>
2021-08-05 19:18:16 +1000

    Implemented annotation validation checks in the controller class

--- src/main/java/com/codingexercise/rest/EmployeeEndpoint.java
+++ src/main/java/com/codingexercise/rest/EmployeeEndpoint.java
@@ -4,9 +4,13 @@ import static org.springframework.http.ResponseEntity.ok;
 
 import java.util.List;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.codingexercise.model.Employee;
 import com.codingexercise.service.EmployeeService;
 
+@Validated
 @RestController
 @RequestMapping("/employee")
 public class EmployeeEndpoint {
@@ -23,7 +28,7 @@ public class EmployeeEndpoint {
 	private EmployeeService employeeService;
 	
 	@PostMapping(value = "payslip", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<ApiResponse> payslip(@RequestBody List<Employee> employees) {
+	public ResponseEntity<ApiResponse> payslip(@RequestBody @NotEmpty(message = "Request cannot be empty list")  List<@Valid Employee> employees) {
 		return ok().body(new ApiResponse(employeeService.getAllMonthlyPayslip(employees),null));
 		
 	}
